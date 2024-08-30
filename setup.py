6bce061494930080b98ef93d33ff740bfcdb4f4f
vazarkevych <volodymyr.nazarkevych@kevychsolutions.com>
2024-08-30 14:58:34 +0300

    Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ test_requirements = ['pytest>=3', ]
 
 setup(
     name='growthbook',
-    version='1.1.0',
+    version='1.2.0',
     author="GrowthBook",
     author_email='hello@growthbook.io',
     python_requires='>=3.6',
